cda307a0504105eb23afb4882847acebcbce1191
jony <joniufc175@gmail.com>
2021-06-01 20:04:13 +0500

    Javokhir

---
+++ src/assets/components/Javokhir001.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Javokhir001 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
